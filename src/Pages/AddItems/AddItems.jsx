97b34ad4d304f625f94592fa9d7530323ad45677
Al-Amin <116742755+adnanalamin@users.noreply.github.com>
2024-04-27 18:42:29 +0600

    Added Filter For My Art & Craft List page

--- src/Pages/AddItems/AddItems.jsx
+++ src/Pages/AddItems/AddItems.jsx
@@ -4,8 +4,8 @@ import { AuthContext } from "../../AuthProvider/AuthProvider";
 
 const AddItems = () => {
     const [catagory, setCatagory] = useState()
-    const [stock, setStock] = useState()
     const [customaization, setCustomaization] = useState()
+    const [stock, setStock] = useState()
 
     const { user} = useContext(AuthContext);
     const handelCatagoryChange = e => {
@@ -120,7 +120,7 @@ const AddItems = () => {
                 <label className="text-sm font-medium text-gray-900 block mb-2">
                   Customization
                 </label>
-                <select onChange={handelStockChange} value={stock} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5">
+                <select  value={customaization} onChange={handelCustomaizationChange} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5">
                   <option value="" disabled selected>
                     Customization
                   </option>
@@ -132,7 +132,7 @@ const AddItems = () => {
                 <label className="text-sm font-medium text-gray-900 block mb-2">
                   Stock Status
                 </label>
-                <select value={customaization} onChange={handelCustomaizationChange} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5">
+                <select onChange={handelStockChange} value={stock} className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-cyan-600 focus:border-cyan-600 block w-full p-2.5">
                   <option value="" disabled selected>
                     Stock Status
                   </option>
--- src/Pages/MyArtAndCraft/MyArtAndCraft.jsx
+++ src/Pages/MyArtAndCraft/MyArtAndCraft.jsx
@@ -1,23 +1,73 @@
 import { useLoaderData } from "react-router-dom";
 import Card from "../../components/Card/Card";
-
+import { FaAnglesDown } from "react-icons/fa6";
+import { useState } from "react";
 
 const MyArtAndCraft = () => {
-    const items = useLoaderData()
-    console.log(items)
-    return (
-        <div className="mt-12">
-            <div className="text-center mb-8">
-                <h1 className="text-4xl font-poppins font-bold">My Art & Craft List</h1>
-                <h3 className="md:w-3/5 md:mx-auto font-inter text-base mt-8 text-[#7F7D7D]">I would be happy to help! Could you provide a bit more detail about what specific types of art and craft items are on your list? For example, are you interested in painting, drawing, knitting, pottery, or any other specific crafts? The more information you provide, the better I can tailor my descriptions to your interests.</h3>
+  const items = useLoaderData();
+  const [myItems, setMyItems] = useState(items)
+  console.log(items);
+
+  const handelCustomizationFilter = (filter) => {
+    if (filter === "all") {
+        setMyItems(items);
+    } else if (filter === "Yes") {
+      const yescustomaization = items.filter((item) => item.customaization == filter);
+      setMyItems(yescustomaization);
+    } else if (filter === "No") {
+      const nocustomaization = items.filter((item) => item.customaization == filter);
+      setMyItems(nocustomaization);
+    }
+  };
+  return (
+    <div className="mt-12">
+      <div className="text-center mb-8">
+        <h1 className="text-4xl font-poppins font-bold">My Art & Craft List</h1>
+        <h3 className="md:w-3/5 md:mx-auto font-inter text-base mt-8 text-[#7F7D7D]">
+          I would be happy to help! Could you provide a bit more detail about
+          what specific types of art and craft items are on your list? For
+          example, are you interested in painting, drawing, knitting, pottery,
+          or any other specific crafts? The more information you provide, the
+          better I can tailor my descriptions to your interests.
+        </h3>
+        <div>
+          <div className="mx-auto flex  w-full items-center justify-center  py-2">
+            <div className="group relative cursor-pointer py-2">
+              <div className="flex items-center justify-between space-x-5 bg-white px-4">
+                <a
+                  className="menu-hover my-2 py-2 text-base font-medium text-black lg:mx-4"
+                  onClick=""
+                >
+                  Customization
+                </a>
+                <span>
+                  <FaAnglesDown className="h-6 w-6"></FaAnglesDown>
+                </span>
+              </div>
+
+              <div className="invisible absolute z-50 flex w-full flex-col bg-gray-100 py-1 px-4 text-gray-800 shadow-xl group-hover:visible">
+                <a onClick={() => handelCustomizationFilter('all')} className="my-2 block border-b border-gray-100 py-1 font-semibold text-gray-500 hover:text-black md:mx-2">
+                  All
+                </a>
+                <a onClick={() => handelCustomizationFilter('Yes')} className="my-2 block border-b border-gray-100 py-1 font-semibold text-gray-500 hover:text-black md:mx-2">
+                  Yes
+                </a>
+
+                <a onClick={() => handelCustomizationFilter('No')} className="my-2 block border-b border-gray-100 py-1 font-semibold text-gray-500 hover:text-black md:mx-2">
+                  No
+                </a>
+              </div>
             </div>
-            <div  className="md:grid md:grid-cols-2 lg:grid-cols-3 lg:gap-5 md:gap-3 gap-2 space-y-4 md:space-y-0 md:mx-8">
-            {
-                items.map(item => <Card key={item._id} item={item}></Card>)
-            }
-            </div>
+          </div>
         </div>
-    );
+      </div>
+      <div className="md:grid md:grid-cols-2 lg:grid-cols-3 lg:gap-5 md:gap-3 gap-2 space-y-4 md:space-y-0 md:mx-8">
+        {myItems.map((item) => (
+          <Card key={item._id} item={item}></Card>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default MyArtAndCraft;
